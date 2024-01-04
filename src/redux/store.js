d39ce7632d9abf70ad8ac28edff39814d58ccca4
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-01-04 15:59:10 -0800

    remove unnecessary state slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import ingredientSliceReducer from './ingredientSlice';
 import recipesSliceReducer from './recipeSlice'
-import recipeInfoSliceReducer from './recipeInfoSlice'
+
 
 export const store = configureStore({
     reducer: {
         ingredient: ingredientSliceReducer,
         recipes: recipesSliceReducer,
-        recipeInfo: recipeInfoSliceReducer
     }
 })
